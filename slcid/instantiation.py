e42b2cbaa65b78e0cf4ffa4c763c3f4842df1f8b
mrbozzo <jlabejof@yahoo.fr>
2013-07-04 17:33:10 +0200

    add instantiation module

---
+++ slcid/instantiation.py
@@ -0,0 +1,18 @@
+from core import MB, Eval
+
+class Instantiator(MB):
+    """
+    Class in charge of instantiate magic boxes when evaluated
+    """
+
+class ParameterType(Instantiator):
+    pass
+
+class Operation(Instantiator):
+    pass
+
+class Type(Instantiator):
+    pass
+
+class TypeDefinition(Eval):
+    pass
